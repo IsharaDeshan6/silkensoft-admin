d3c1edd066c80e36400109f839f63adc726fe7a0
P . A Ishara Deshan Appuhami <110235551+IsharaDeshan6@users.noreply.github.com>
2025-02-26 10:26:03 +0530

    5th commit change the localhost to static domain

--- hooks/useSignIn.ts
+++ hooks/useSignIn.ts
@@ -19,7 +19,6 @@ const useSignIn = () => {
 
             if (response.ok) {
                 const data = await response.json();
-                console.log(data);
                 if (data.message === "success") {
                     localStorage.setItem('token', data.token);
                     localStorage.setItem('userType', data.userType);
